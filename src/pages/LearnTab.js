b377c86150b4658c396422c7e71da9f30221b172
best-developer-in-africa <wilsonajikwu12@gmail.com>
2022-10-20 23:28:52 +0100

    update

--- src/pages/LearnTab.js
+++ src/pages/LearnTab.js
@@ -87,7 +87,7 @@ function LearnTab() {
                         /> */}
                         <object 
                             width="100%"
-                            height="100vh"
+                            height="900"
                             data={`https://docs.google.com/gview?embedded=true&url=${pdf}`}
                         />
                         {/* <div className="video-play-bar">
